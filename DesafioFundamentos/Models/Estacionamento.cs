39f3271357bbc7102fc862a880fae3174c25d922
Isaias Junior <isaiscastro56@gmail.com>
2024-08-12 09:32:50 -0300

    Correção no campo de remover veiculo e implementação do campo de listar veiculos

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -14,7 +14,6 @@ namespace DesafioFundamentos.Models
 
         public void AdicionarVeiculo()
         {
-            // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
             Console.WriteLine("Digite a placa do veículo para estacionar:");
             veiculos.Add(Console.ReadLine());
 
@@ -33,14 +32,10 @@ namespace DesafioFundamentos.Models
             {
                 Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
 
-                // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
-                // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal
-                Console.WriteLine("Por favor digite a quantidade de horas que o veiculo permaneceu estacionado");
                 int horas = Convert.ToInt32(Console.ReadLine());
 
-                decimal valorTotal = (precoInicial + precoPorHora) * horas; 
+                decimal valorTotal = precoInicial + precoPorHora * horas; 
 
-                // TODO: Remover a placa digitada da lista de veículos
                 veiculos.Remove(placa);
 
                 Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
@@ -57,8 +52,11 @@ namespace DesafioFundamentos.Models
             if (veiculos.Any())
             {
                 Console.WriteLine("Os veículos estacionados são:");
-                // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
-                // *IMPLEMENTE AQUI*
+                int contador = 0;
+                foreach (string item in veiculos) {
+                    Console.WriteLine($"{veiculos[contador]}");
+                    contador++;
+                }
             }
             else
             {
